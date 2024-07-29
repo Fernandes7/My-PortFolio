9cf2593a6ea7c564e5ecf449e0c87b68e4b6c803
Fernandes K J <fernandeskj007@gmail.com>
2024-07-29 20:00:29 +0530

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Loader from './Components/Loader/Loader'
+import MainRoutes from './MainRoutes'
+
+function App() {
+  return (
+   <div>
+    <MainRoutes />
+    <Loader />
+   </div>
+  )
+}
+
+export default App
